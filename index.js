474b16bdb5e1f011d08ee17c2449189af18bfdcc
Krish Agarwal <116301754+krishagarwal1907@users.noreply.github.com>
2023-05-09 15:42:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementById('toggle')
+const navlinks = document.getElementById('links')
+toggleButton.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
